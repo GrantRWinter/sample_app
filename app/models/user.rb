8723fbb0696495e5585f49c1a20fab858fd8d21c
GrantRWinter <grantrwinter@gmail.com>
2014-07-31 16:32:04 -0600

    make a basic User modal (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true
+                    uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+  #presence validations for the password and its confirmations are automatically added by "has_secure_password"
+
+  has_secure_password
 end
