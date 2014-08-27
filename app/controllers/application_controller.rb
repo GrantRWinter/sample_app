8f202b3cefe7307782fa75c40c1316d8f66a27d6
GrantRWinter <grantrwinter@gmail.com>
2014-08-26 21:08:45 -0600

    added form_tag vs form_for for sessions_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+  # ************** http basic authentication below ************
+  #http_basic_authenticate_with name: "Grant", password: "Winter"
   protect_from_forgery with: :exception
   include SessionsHelper
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    # ************ way to use if using form for exercise 1 chapter 8 *******
+    # user = User.find_by(email: params[:session][:email].downcase)
+    # if user && user.authenticate(params[:session][:password])
+    #     sign_in user
+    #     redirect_to user
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
         sign_in user
         redirect_to user
     else
